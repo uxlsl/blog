c740f416939103392190cf0e96f3527a47cee1c4
ux_lsl <ux_lsl@163.com>
2016-01-06 19:27:16 +0800

    增加电源模型

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,21 @@
+# -*- coding:utf-8 -*-
+from __future__ import division, absolute_import
+from __future__ import print_function, unicode_literals
+
+import json
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    name = models.CharField(unique=True, max_length=128, verbose_name="名称")
+    url = models.URLField(verbose_name="所在网址")
+    source = models.CharField(max_length=128, verbose_name="下载来源")
+    down_urls = models.TextField(verbose_name="下载地址,多个数据json)")
+    create_at = models.DateTimeField(auto_now=True,
+                                     verbose_name="时间")
+    update_at = models.DateTimeField(auto_now=True,
+                                     verbose_name="更新时间")
+
+    @property
+    def download_urls(self):
+        return json.loads(self.down_urls)
--- movie/views.py
+++ movie/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'movie_list.html')
+    content = {'object_list': [{
+        'name': 'lsl',
+        'source': 'abc',
+        'url': 'www.qq.com',
+        'down_urls': [1, 2, 3, 4, 5]
+    }]}
+    return render(request, 'movie_list.html', content)
