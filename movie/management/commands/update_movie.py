808d38e11947aba2a987bfb9b9736f13cc9408ba
ux_lsl <ux_lsl@163.com>
2016-01-08 22:10:31 +0800

    修改资源的结构

--- movie/management/commands/update_movie.py
+++ movie/management/commands/update_movie.py
@@ -2,9 +2,13 @@
 import os
 import json
 from django.db import IntegrityError, transaction
-from django.core.exceptions import FieldDoesNotExist, FieldError
+from django.core.exceptions import (
+    FieldDoesNotExist,
+    FieldError,
+    ObjectDoesNotExist
+)
 from django.core.management.base import BaseCommand, CommandError
-from movie.models import Movie
+from movie.models import Movie, MovieRes
 
 
 class Command(BaseCommand):
@@ -14,17 +18,19 @@ class Command(BaseCommand):
         parser.add_argument('path', nargs='+', type=str)
 
     def handle(self, *args, **option_list):
-        "更新"
         if (option_list['path']
                 and os.path.isfile(option_list['path'][0])):
             try:
                 data = json.load(open(option_list['path'][0]))
                 with transaction.atomic():
                     for item in data:
-                        del item['_type']
-                        item['down_urls'] = json.dumps(item['down_urls'])
-                        Movie.objects.update_or_create(name=item['name'],
-                                                       defaults=item)
-            except (ValueError, IntegrityError,
+                        try:
+                            movie = Movie.objects.get(name=item.pop('name'))
+                            MovieRes.objects.update_or_create(movie=movie,
+                                                              url=item['url'],
+                                                              defaults=item)
+                        except ObjectDoesNotExist:
+                            pass
+            except (KeyError, IntegrityError,
                     FieldDoesNotExist, FieldError) as e:
                 raise CommandError(e.message)
--- movie/models.py
+++ movie/models.py
@@ -1,4 +1,7 @@
 # -*- coding:utf-8 -*-
+# https://docs.djangoproject.com/en/1.9/topics/db/examples/many_to_one/
+
+
 from __future__ import division, absolute_import
 from __future__ import print_function, unicode_literals
 
@@ -7,7 +10,29 @@ from django.db import models
 
 
 class Movie(models.Model):
+    """电影基本信息
+    """
     name = models.CharField(unique=True, max_length=128, verbose_name="名称")
+    filmtype = models.CharField(max_length=32, blank=True, null=True,
+                                verbose_name="分类")
+    director = models.CharField(max_length=128, blank=True, null=True,
+                                verbose_name="导演")
+    starring = models.CharField(max_length=128,
+                                blank=True,
+                                null=True, verbose_name="主演")
+    create_at = models.DateTimeField(auto_now=True,
+                                     verbose_name="时间")
+    update_at = models.DateTimeField(auto_now=True,
+                                     verbose_name="更新时间")
+
+
+class MovieRes(models.Model):
+    """电影资源地址
+    """
+    movie = models.ForeignKey(Movie,
+                              models.SET_NULL,
+                              blank=True,
+                              null=True)
     url = models.URLField(verbose_name="所在网址")
     source = models.CharField(max_length=128, verbose_name="下载来源")
     down_urls = models.TextField(verbose_name="下载地址,多个数据json)")
@@ -16,6 +41,17 @@ class Movie(models.Model):
     update_at = models.DateTimeField(auto_now=True,
                                      verbose_name="更新时间")
 
-    @property
-    def download_urls(self):
-        return json.loads(self.down_urls)
+    def foo():
+        doc = """Doc string"""
+
+        def fget(self):
+            return json.loads(self.down_urls)
+
+        def fset(self, value):
+            self.down_urls = json.dumps(value)
+
+        def fdel(self):
+            del self._down_urls
+        return locals()
+
+    download_urls = property(**foo())
