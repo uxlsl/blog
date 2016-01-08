1460a93c91eb3a9c9480d9013e4b6363c5c7a5e5
ux_lsl <ux_lsl@163.com>
2016-01-08 22:19:55 +0800

    修改查询逻辑

--- movie/views.py
+++ movie/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from .models import Movie
+from django.db.models import Q
+from .models import MovieRes
 
 
 class MovieList(ListView):
     template_name = "movie_list.html"
     context_object_name = "movie_list"
-    queryset = Movie.objects.all()
     paginate_by = 10
 
     def get_queryset(self):
         if 'q' in self.request.GET:
-            queryset = Movie.objects.filter(
-                name__contains=self.request.GET['q']).order_by('-update_at')
+            queryset = MovieRes.objects.filter(
+                Q(movie__name__contains=self.request.GET['q'])
+                | Q(movie__starring__contains=self.request.GET['q'])
+            ).order_by('-update_at')
         else:
-            queryset = Movie.objects.order_by('-update_at')
+            queryset = MovieRes.objects.order_by('-update_at')
         return queryset
